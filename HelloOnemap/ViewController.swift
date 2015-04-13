6978d17fad220048a2d1b07a4bf83c9db88c05dd
zkkmin <zkkmin@gmail.com>
2015-04-13 21:18:24 +0800

    Display selected search result on the map by adding to graphic layer.

--- HelloOnemap/ViewController.swift
+++ HelloOnemap/ViewController.swift
@@ -9,18 +9,26 @@
 import UIKit
 import ArcGIS
 
+struct Place {
+    var placeName = ""
+    var Category = ""
+    var X = 0.0
+    var Y = 0.0
+}
+
 
 class ViewController: UIViewController, AGSMapViewLayerDelegate, UISearchResultsUpdating, UITableViewDelegate, UITableViewDataSource, UISearchControllerDelegate, UISearchBarDelegate {
 
     @IBOutlet weak var mapView: AGSMapView!
-    
+    var graphicLayer = AGSGraphicsLayer()
     
     let xmin = 29495.9472786567
     let ymin = 39801.9418330241
     let xmax = 30037.5707551916
     let ymax = 40765.3094566208
     
-    var data = ["Cityhall", "CityCenter", "CityShit"]
+    var data = [Place]()
+    let session = NSURLSession.sharedSession()
     
     var resultTableViewController = UITableViewController()
     var searchController = UISearchController()
@@ -55,34 +63,42 @@ class ViewController: UIViewController, AGSMapViewLayerDelegate, UISearchResults
         // add base map
         let url = NSURL(string: "http://e1.onemap.sg/arcgis/rest/services/BASEMAP/MapServer")
         let tiledLayer = AGSTiledMapServiceLayer(URL: url)
-        self.mapView.addMapLayer(tiledLayer, withName: "Basemap Tiled Layer")
-        self.mapView.layerDelegate = self
+        mapView.addMapLayer(tiledLayer, withName: "Basemap Tiled Layer")
+        mapView.layerDelegate = self
+        mapView.addMapLayer(graphicLayer, withName: "graphicLayer")
+        let envelope = AGSEnvelope(xmin: xmin, ymin: ymin, xmax: xmax, ymax: ymax, spatialReference: mapView.spatialReference);
+        mapView.zoomToEnvelope(envelope, animated: false)
         
-        //let envelope = AGSEnvelope(xmin: xmin, ymin: ymin, xmax: xmax, ymax: ymax, spatialReference: self.mapView.spatialReference);
-        //self.mapView.zoomToEnvelope(envelope, animated: false)
+    }
+    
+    func showSelectedSearchPlace(selectedPlace :Place){
+        searchController.active = false
+        searchController.searchBar.text = selectedPlace.placeName
+        
+        let myMarkerSymbol = AGSSimpleMarkerSymbol()
+        myMarkerSymbol.color = UIColor.redColor()
+        
+        let point = AGSPoint(x: selectedPlace.X, y: selectedPlace.Y, spatialReference: mapView.spatialReference)
+        mapView.centerAtPoint(point, animated: false)
+        
+        var pointGraphic = AGSGraphic(geometry: point, symbol: myMarkerSymbol, attributes: nil)
+        graphicLayer.removeAllGraphics()
+        graphicLayer.addGraphic(pointGraphic)
         
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        searchController.searchBar.text = data[indexPath.row]
-        
-        let searchText = searchController.searchBar.text
-        searchController.active = false
-        searchController.searchBar.text = searchText
-        
-        
-    }
-    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
-        println("click");
-        
-        let searchText = searchController.searchBar.text
-        searchController.active = false
-        searchController.searchBar.text = searchText
-
+        searchController.searchBar.text = data[indexPath.row].placeName
+        showSelectedSearchPlace(data[indexPath.row])
     }
     
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        //showSelectedSearchPlace()
+    }
+    
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let rowData = data[indexPath.row]
+        let rowData = data[indexPath.row].placeName
         let cell = UITableViewCell()
         cell.textLabel?.text = rowData
         return cell
@@ -96,32 +112,54 @@ class ViewController: UIViewController, AGSMapViewLayerDelegate, UISearchResults
     
     func updateSearchResultsForSearchController(searchController: UISearchController) {
         
-        getAddresses("")
+        getAddresses(searchController.searchBar.text)
         
     }
     
     // Call OneMap's Address Search API to search a location in Singapore
     func getAddresses(keyWord: String ){
-        let urlString = "http://www.onemap.sg/API/services.svc/basicSearch?token=qo/s2TnSUmfLz+32CvLC4RMVkzEFYjxqyti1KhByvEacEdMWBpCuSSQ+IFRT84QjGPBCuz/cBom8PfSm3GjEsGc8PkdEEOEr&wc=SEARCHVAL%20LIKE%20%27CITY$%27&otptFlds=CATEGORY&returnGeom=0&nohaxr=10"
         
-        let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
         
-        session.dataTaskWithURL(NSURL(string: urlString)!, completionHandler: { (taskData, taskResponse, taskError) -> Void in
-            var jsonReadError : NSError?
-            var jsonResult = NSJSONSerialization.JSONObjectWithData(taskData, options: NSJSONReadingOptions.MutableContainers, error: &jsonReadError) as NSDictionary
+        if keyWord != "" {
+            let keyWordEscaped = keyWord.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
             
-            let resultsArray = jsonResult["SearchResults"] as NSArray
+            let urlString = "http://www.onemap.sg/API/services.svc/basicSearch?token=qo/s2TnSUmfLz+32CvLC4RMVkzEFYjxqyti1KhByvEacEdMWBpCuSSQ+IFRT84QjGPBCuz/cBom8PfSm3GjEsGc8PkdEEOEr&wc=SEARCHVAL%20LIKE%20%27\(keyWordEscaped!)$%27&otptFlds=CATEGORY&returnGeom=0&nohaxr=10"
             
-            for (index, result) in enumerate(resultsArray){
-                if index > 0 {
-                    if let placeName = result["SEARCHVAL"] as String? {
-                        self.data.append(placeName)
+            session.dataTaskWithURL(NSURL(string: urlString)!, completionHandler: { (taskData, taskResponse, taskError) -> Void in
+                println("\(urlString)")
+                var jsonReadError : NSError?
+                var jsonResult = NSJSONSerialization.JSONObjectWithData(taskData, options: NSJSONReadingOptions.MutableContainers, error: &jsonReadError) as NSDictionary
+                
+                let resultsArray = jsonResult["SearchResults"] as NSArray
+                self.data = []
+                for (index, result) in enumerate(resultsArray){
+                    if index > 0 {
+                        var place = Place()
+                        
+                        if let placeName = result["SEARCHVAL"] as String? {
+                            place.placeName = placeName
+                        }
+                        
+                        if let category = result["CATEGORY"] as String? {
+                            place.Category = category
+                        }
+                        
+                        if let x = result["X"] as NSString? {
+                            place.X = x.doubleValue
+                        }
+                        
+                        if let y = result["Y"] as NSString? {
+                            place.Y = y.doubleValue
+                        }
+                        
+                        self.data.append(place)
                     }
                 }
-            }
-
-            self.resultTableViewController.tableView.reloadData()
-        }).resume()
+                
+                self.resultTableViewController.tableView.reloadData()
+            }).resume()
+        }
+        
     }
     
     func mapViewDidLoad(mapView:AGSMapView!){
@@ -132,7 +170,8 @@ class ViewController: UIViewController, AGSMapViewLayerDelegate, UISearchResults
     
     func responseToEventChanged(notification: NSNotification){
         let theString = "xmin = \(mapView.visibleAreaEnvelope.xmin),\nymin = \(mapView.visibleAreaEnvelope.ymin),\nxmax = \(mapView.visibleAreaEnvelope.xmax),\nymax = \(mapView.visibleAreaEnvelope.ymax)"
-        println(theString);
+        // println(theString);
+        println("sale \(mapView.mapScale)")
     }
 
 }
